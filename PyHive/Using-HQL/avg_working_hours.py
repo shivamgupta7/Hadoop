80888c48d10971ad55ac84cad05d88e3d7879188
Shivam Gupta <shivamg.cr7@gmail.com>
2020-05-19 14:00:34 +0530

    [Shivam] Find each user average working hours in 21,22,23,24 dates.

---
+++ PyHive/Using-HQL/avg_working_hours.py
@@ -0,0 +1,24 @@
+from pyhive import hive
+import pandas as pd
+import csv
+
+conn = hive.Connection(host="localhost",port = 10000, username="hadoopuser",database = 'logs',auth='NOSASL')
+
+try:
+    cursor = conn.cursor()
+    cursor.execute('SELECT user_name, from_unixtime(CAST(AVG(unix_timestamp(substr(working_hours,12),"HH:mm:ss"))as bigint),"HH:mm:ss") as avg_hours FROM workinglogs1 GROUP BY user_name ORDER BY avg_hours')
+    result = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    out_file = open('output/avg_working_hours.csv', 'w')
+    myFile = csv.writer(out_file)
+    myFile.writerow(column_names)
+    myFile.writerows(result)
+    out_file.close()
+    print('Query run successfully.')
+except hive.Error as err:
+    print('Syntax error')
+    print(err)
+finally:
+    cursor.close()
+    conn.close()
+    print("Connection is closed")
