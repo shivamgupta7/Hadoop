02b78e82d3a6b13bf9e155d1043b58da23a83bb2
Shivam Gupta <shivamg.cr7@gmail.com>
2020-05-26 22:58:16 +0530

    [Shivam] Find average working hours for each users. And then find who is working less then total average hour.

--- PySpark/Queries.py
+++ PySpark/Queries.py
@@ -18,3 +18,19 @@ class SparkQuery:
         total_avg_work_hour = df_avg_hours.select(avg('avg_hour')).collect()[0][0]
         high_working_users = df_avg_hours.filter(df_avg_hours['avg_hour'] > total_avg_work_hour).select('user_name')
         return high_working_users
+
+    def lowest_avg_working_user(self,df):
+        '''
+        Find average working hours for each users. And then find who is working less then total average hour.
+        '''
+        df_work = df.drop('idle_time', 'start_time', 'end_time')  # only take working hour column
+        # Find average working hour for each user
+        df_avg=df_work.groupBy('user_name').agg(sqlFun.from_unixtime(sqlFun.avg(sqlFun.unix_timestamp('working_hour')),'hh:mm:ss').alias('avg_time'))
+        # Convert all into hour
+        df_avg_hours = df_avg.withColumn('avg_hour', (hour(df_avg['avg_time'])*3600 + minute(df_avg['avg_time'])*60 + second(df_avg['avg_time']))/3600)
+        #calculating average hours
+        total_avg_work_hour = df_avg_hours.select(avg('avg_hour')).collect()[0][0]
+        lowest_working_users = df_avg_hours.filter(df_avg_hours['avg_hour'] < total_avg_work_hour).select('user_name')
+        return lowest_working_users
+
+    
