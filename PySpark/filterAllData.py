1066e5d026fb4a3cb05be183c3750a3834003225
Shivam Gupta <shivamg.cr7@gmail.com>
2020-05-26 18:46:12 +0530

    [Shivam] In this function doing all operation(like:find start_time, end_time, join tables and uniun all tables and return all combine dataframe into one dataframe

--- PySpark/filterAllData.py
+++ PySpark/filterAllData.py
@@ -50,3 +50,59 @@ def cal_working_hours(details,date,initial_time,users_data):
     users_data[details.user_name]['working_hour'] = dt.datetime.strptime(date + initial_time, '%Y-%m-%d %H:%M:%S') + delta
     users_data[details.user_name]['start_time'] = details.start_time
     users_data[details.user_name]['end_time'] = details.end_time
+
+def complete_operation(directory, start_timing, end_timing, initial_time, df_total):
+    '''
+    In this function doing all operation(like:find start_time, end_time, join tables and uniun all tables and return all combine dataframe into one dataframe
+    '''
+    try:
+        paths = show_files(directory)
+        users_data = dict()
+        for path in paths:
+            path = path.split()[-1]
+            log = read_file(path,header=True,inferSchema=True)
+            select_column = ['DateTime', 'user_name', 'keyboard', 'mouse']
+            log = log.select(*select_column)
+            date = dt.datetime.strftime(log.collect()[0][0],'%Y-%m-%d')
+            print('Start ' + date)
+            log = log[log['DateTime'] >= date + start_timing]
+            log = log[log['DateTime'] <= date + end_timing]
+            unique_users = log.select('user_name').distinct().rdd.map(lambda r: r[0]).collect()
+            start_DateTime = dt.datetime.strptime(date + initial_time, '%Y-%m-%d %H:%M:%S')
+    #         print(start_DateTime)
+            for user in unique_users:
+                users_data[user] = {
+                'start_time' : start_DateTime,
+                'end_time' : start_DateTime,
+                'idle_time': start_DateTime,
+                'working_hour' : start_DateTime
+                }
+            log = log.sort('DateTime')
+            startTime = log.groupBy("user_name").agg(sqlFun.min("DateTime").alias('start_time'))
+            # print(startTime.show(5))
+            endTime = log.groupBy("user_name").agg(sqlFun.max("DateTime").alias('end_time'))
+            # print(endTime.show(5))
+            for user in unique_users:
+                count_idle = 0
+                for row in log.rdd.collect():
+                    if user == row.user_name:
+                        cal_idle_hour(row,users_data)
+            # print('Done! Idle hour.')
+            data_rows_idle = [Row(**{'user_name': user, **logs}) for user,logs in users_data.items()]
+            idleTime = spark.createDataFrame(data_rows_idle).select('user_name', 'idle_time')
+            # print(idleTime.show(5))
+            data_df = idleTime.join(startTime, on=['user_name'], how='inner').join(endTime, on=['user_name'], how='inner')
+            # data_df.show(5)
+            for row in data_df.rdd.collect():
+                cal_working_hours(row, date, initial_time,users_data)
+            # print('Done! Working hour.')
+            data_as_rows = [Row(**{'user_name': user, **logs}) for user,logs in users_data.items()]
+            data_final = spark.createDataFrame(data_as_rows).select('user_name', 'start_time', 'end_time', 'idle_time', 'working_hour')
+            # data_final.show(5)
+            print('Done '+ date)
+            df_total = df_total.unionAll(data_final)
+            users_data={}
+        print('Done!')
+        return df_total
+    except Exception as err:
+        print(err)
