34104490fd6931516fda44af3b5811f8b7a82f67
Shivam Gupta <shivamg.cr7@gmail.com>
2020-05-26 23:01:38 +0530

    [Shivam] Find average idle hours for each users. And then find who is idle less then total average hour.

--- PySpark/Queries.py
+++ PySpark/Queries.py
@@ -47,3 +47,16 @@ class SparkQuery:
         high_idle_users = df_avg_hours.filter(df_avg_hours['avg_hour'] > total_avg_idle_hour).select('user_name')
         return high_idle_users
     
+    def lowest_avg_idle_user(self, df):
+        '''
+        Find average idle hours for each users. And then find who is idle less then total average hour.
+        '''
+        df_idle = df.drop('working_hour', 'start_time', 'end_time')  # only take working hour column
+        # Find average idle hour for each user
+        df_avg=df_idle.groupBy('user_name').agg(sqlFun.from_unixtime(sqlFun.avg(sqlFun.unix_timestamp('idle_time')),'hh:mm:ss').alias('avg_time'))
+        # Convert all into hour
+        df_avg_hours = df_avg.withColumn('avg_hour', (hour(df_avg['avg_time'])*3600 + minute(df_avg['avg_time'])*60 + second(df_avg['avg_time']))/3600)
+        #calculating average hours
+        total_avg_idle_hour = df_avg_hours.select(avg('avg_hour')).collect()[0][0]
+        lowest_idle_users = df_avg_hours.filter(df_avg_hours['avg_hour'] < total_avg_idle_hour).select('user_name')
+        return lowest_idle_users
